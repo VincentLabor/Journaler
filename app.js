c71080985645d44bf4c56a2b33f291c09e0346f2
VincentLabor <vlabor95@gmail.com>
2019-06-16 23:42:17 -0700

    Header is started

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const ejs = require("ejs");
 const app = express();
 app.set('view engine', 'ejs');
 
+app.use(express.static("public"));
+
 app.get("/", (req,res) => res.render("home"));
+
 
 app.listen(3000, ()=> console.log(`The server is now online`));
