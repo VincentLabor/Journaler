433ed38f2b96bed7511c9dcee93943902ee0d6ee
VincentLabor <vlabor95@gmail.com>
2019-06-17 22:53:25 -0700

    Styled the login and Register and added Mongoose

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var mongoose = require("mongoose");
 
 const app = express();
-app.set('view engine', 'ejs');
-
 app.use(express.static("public"));
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+mongoose.connect('mongodb://localhost/blogDB', {useNewUrlParser: true})
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("We are connected to the database!")
+});
+
+app.set('view engine', 'ejs');
 
 app.get("/", (req,res) => res.render("home"));
 
